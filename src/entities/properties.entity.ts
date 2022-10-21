aff746f16e4a81a56b2767703a289f2f9f692e9f
GeovaneSO <oliveira.5geovane@outlook.com>
2022-10-21 15:55:43 -0300

    feat/creating entities and migrations

--- src/entities/properties.entity.ts
+++ src/entities/properties.entity.ts
@@ -1,5 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from "typeorm";
-
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, OneToOne, JoinColumn, OneToMany, ManyToOne } from "typeorm";
+import Decimal from 'decimal.js'
+import Addresses from "./addresses.entity";
+import Schedules_user_properties from "./schedules_user_properties.entity";
+import Categories from "./categories.entity";
 @Entity('properties')
 class Properties{
     @PrimaryGeneratedColumn('uuid')
@@ -8,5 +11,32 @@ class Properties{
     @Column({name: 'sold', default: false})
     sold: boolean;
 
-    @Column({name: 'value', type: 'decimal', })
+    @Column({name: 'value', type: 'decimal', precision: 12, scale: 2})
+    value: number;
+
+    @Column({type: 'integer', nullable: true})
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @OneToOne((type) => Addresses, {
+        eager: true
+    })
+
+    @JoinColumn()
+    addresses: Addresses;
+
+    @OneToMany(() => Schedules_user_properties, schedules => schedules.property, {
+        eager: true
+    })
+    schedules: Schedules_user_properties[]
+    
+    @ManyToOne(() => Categories, (category) => category.properties)
+    @JoinColumn()
+    category: Categories;
+    
 }
+export default Properties;
